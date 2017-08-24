508ad9e86d953e8d0bae15e2c1a67ab237e3acf6
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-08-24 11:14:58 +0200

    Add idiom to read lines from a file.

---
+++ read_lines_from_file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::ifstream file("data.txt");
+    std::string line;
+    // See: https://stackoverflow.com/q/7219062/496459
+    // std::getline has no buffer-size limitation, per the standard.
+    while(std::getline(file, line))
+    {
+        std::cout << line << '\n';
+    }
+
+    return 0;
+}
+
